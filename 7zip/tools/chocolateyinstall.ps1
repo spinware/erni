05430034c1a13eefd677dc5304c2b79a44047a3a
spinware <spinware@gmail.com>
2019-05-21 17:55:13 +0800

    Add files via upload

---
+++ 7zip/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$fileLocation = Join-Path $toolsDir '7z1900-x64.exe'
+
+$packagename = '7zip'
+
+$packageArgs = @{
+  packageName   = $packagename
+  #unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  #url           = $url
+  #url64bit      = $url64
+  file         = $fileLocation
+
+  softwareName  = '7zip*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0)
+  
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
---
+++ 7zip/tools/chocolateyuninstall.ps1
@@ -0,0 +1,48 @@
+﻿
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$fileLocation = Join-Path $toolsDir '7z1900-x64.exe'
+
+$packagename = '7zip'
+
+$packageArgs = @{
+  packageName   = $packagename
+  #unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  #url           = $url
+  #url64bit      = $url64
+  file         = $fileLocation
+
+  softwareName  = '7zip*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  # MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0)
+  
+}
+
+$uninstalled = $false
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString)" #NOTE: You may need to split this if it contains spaces, see below
+    
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+     
+      $packageArgs['file'] = ''
+    } 
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
