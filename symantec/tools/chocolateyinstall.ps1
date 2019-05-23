51201e38053565f912b5b1f618a174acfaf3e05f
spinware <spinware@gmail.com>
2019-05-23 11:32:35 +0800

    Add files via upload

---
+++ symantec/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿    
+$ErrorActionPreference = 'Stop';
+$packageName= '{{PackageName}}'
+$url        = 'https://github.com/spinware/erni/blob/master/SymantecExtractor.exe?raw=true' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  silentArgs    = '/S'
+  url           = $url
+  Checksum 		= '{{checksum}}'
+  ChecksumType 	= 'sha256'
+}
+Stop-Process -ProcessName symantec* -ErrorAction SilentlyContinue -Force
+Install-ChocolateyPackage @packageArgs
